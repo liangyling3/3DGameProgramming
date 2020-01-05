4f7cfbf23dc8c4642e1de26cb2e70fbac5c60626
liangyling3 <34746259+liangyling3@users.noreply.github.com>
2020-01-05 22:45:59 +0800

    。

---
+++ HW10_AITank/Assets/Scripts/Wheel_Control_CS.cs
@@ -0,0 +1,272 @@
+﻿using UnityEngine;
+using System.Collections;
+
+#if UNITY_ANDROID || UNITY_IPHONE
+using UnityStandardAssets.CrossPlatformInput;
+#endif
+
+// This script must be attached to "MainBody".
+namespace ChobiAssets.KTP
+{
+	
+	public class Wheel_Control_CS : MonoBehaviour
+	{
+		[ Header ("Driving settings")]
+		[ Tooltip ("Torque added to each wheel.")] public float wheelTorque = 3000.0f; // Reference to "Wheel_Rotate".
+		[ Tooltip ("Maximum Speed (Meter per Second)")] public float maxSpeed = 7.0f; // Reference to "Wheel_Rotate".
+		[ Tooltip ("Rate for ease of turning."), Range (0.0f, 2.0f)] public float turnClamp = 0.8f;
+		[ Tooltip ("'Solver Iteration Count' of all the rigidbodies in this tank.")] public int solverIterationCount = 7;
+
+		// Reference to "Wheel_Rotate".
+		[HideInInspector] public float leftRate;
+		[HideInInspector] public float rightRate;
+
+		Rigidbody thisRigidbody;
+		bool isParkingBrake = false;
+		float lagCount;
+		float speedStep;
+		float autoParkingBrakeVelocity = 0.5f;
+		float autoParkingBrakeLag = 0.5f;
+
+		ID_Control_CS idScript;
+
+		/* for reducing Calls.
+		Wheel_Rotate_CS[] rotateScripts;
+		*/
+
+		void Awake ()
+		{
+			this.gameObject.layer = 11; // Layer11 >> for MainBody.
+			thisRigidbody = GetComponent < Rigidbody > ();
+			thisRigidbody.solverIterations = solverIterationCount;
+			/* for reducing Calls.
+			rotateScripts = GetComponentsInChildren <Wheel_Rotate_CS> ();
+			*/
+		}
+
+		void Update ()
+		{   
+			if (idScript.isPlayer) {
+				#if UNITY_ANDROID || UNITY_IPHONE
+				Mobile_Input ();
+				#else
+				Desktop_Input ();
+				#endif
+			}
+            else
+            {
+                RaycastHit hit;
+                Vector3 target = new Vector3(0,0,0);
+                float i = 1.0f;
+                bool flag = false;
+
+                //当检测到时，需要break去进行操作
+                while(i >= -1.0f)
+                {
+                    Vector3 _dir = new Vector3(1, 0, 0);
+                    _dir.z += i;
+                    i -= 0.01f;
+                    if (Physics.Raycast(transform.position, _dir, out hit))
+                    {
+                        if (hit.rigidbody != null)
+                        {
+                            //Debug.Log(hit.rigidbody.gameObject.tag);
+                            //Debug.Log(hit.rigidbody.gameObject.transform.position);
+                            if (hit.rigidbody.gameObject.tag == "Player")
+                            {
+                                flag = true;
+                                target = hit.rigidbody.gameObject.transform.position;
+                                break;
+                            }
+                            
+                        }
+                    }
+                }
+
+                if(flag)
+                {
+                    Vector3 localPositionOfTarget = this.transform.InverseTransformPoint(target);
+                    //确定好运动的方向
+                    if(localPositionOfTarget.x > 0)
+                    {
+                        idScript.rightButton = true;
+                        idScript.leftButton = false;
+                    }
+                    else if(localPositionOfTarget.x < 0)
+                    {
+                        idScript.rightButton = false;
+                        idScript.leftButton = true;
+                    }
+                    else
+                    {
+                        idScript.rightButton = false;
+                        idScript.leftButton = false;
+                    }
+
+                    //确定是否前进与开火
+                    var dis = Vector3.Distance(target, this.transform.position);
+                    if(dis > 50)
+                    {
+                        idScript.upButton = true;
+                        idScript.fireButton = false;
+                    }
+                    else
+                    {
+                        idScript.upButton = false;
+                        idScript.fireButton = true;
+                    }
+                }
+                else
+                {   
+
+                    idScript.fireButton = false;
+                    idScript.leftButton = true;
+                    idScript.rightButton = false;
+                    idScript.upButton = false;
+                }
+
+                AI_Input();
+            }
+		}
+
+#if UNITY_ANDROID || UNITY_IPHONE
+		void Mobile_Input ()
+		{
+			if (CrossPlatformInputManager.GetButtonDown ("Up")) {
+				speedStep += 0.5f;
+				speedStep = Mathf.Clamp (speedStep, -1.0f, 1.0f);
+			} else if (CrossPlatformInputManager.GetButtonDown ("Down")) {
+				speedStep -= 0.5f;
+				speedStep = Mathf.Clamp (speedStep, -1.0f, 1.0f);
+			}
+			float vertical = speedStep;
+			float horizontal = 0.0f;
+			if (CrossPlatformInputManager.GetButton ("Left")) {
+				horizontal = Mathf.Lerp (-turnClamp, -1.0f, Mathf.Abs (vertical / 1.0f));
+			} else if (CrossPlatformInputManager.GetButton ("Right")) {
+				horizontal = Mathf.Lerp (turnClamp, 1.0f, Mathf.Abs (vertical / 1.0f));
+			}
+			if (vertical < 0.0f) {
+				horizontal = -horizontal; // like a brake-turn.
+			}
+			leftRate = Mathf.Clamp (-vertical - horizontal, -1.0f, 1.0f);
+			rightRate = Mathf.Clamp (vertical - horizontal, -1.0f, 1.0f);
+		}
+#else
+
+
+        void AI_Input()
+        {
+            if (idScript.upButton)
+            {
+                speedStep += 0.5f;
+                speedStep = Mathf.Clamp(speedStep, -1.0f, 1.0f);
+            }
+            else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+            {
+                speedStep -= 0.5f;
+                speedStep = Mathf.Clamp(speedStep, -1.0f, 1.0f);
+            }
+            else if (Input.GetKeyDown(KeyCode.X))
+            {
+                speedStep = 0.0f;
+            }
+            float vertical = speedStep;
+            //float horizontal = Input.GetAxis("Horizontal");
+            float horizontal = 0;
+            if (idScript.leftButton) horizontal = -0.05f;
+            if (idScript.rightButton) horizontal = 0.05f;
+
+            float clamp = Mathf.Lerp(turnClamp, 1.0f, Mathf.Abs(vertical / 1.0f));
+            horizontal = Mathf.Clamp(horizontal, -clamp, clamp);
+            if (vertical < 0.0f)
+            {
+                horizontal = -horizontal; // like a brake-turn.
+            }
+            leftRate = Mathf.Clamp(-vertical - horizontal, -1.0f, 1.0f);
+            rightRate = Mathf.Clamp(vertical - horizontal, -1.0f, 1.0f);
+        }
+
+        void Desktop_Input ()
+		{
+			if (Input.GetKeyDown (KeyCode.UpArrow) || Input.GetKeyDown (KeyCode.W)) {
+				speedStep += 0.5f;
+				speedStep = Mathf.Clamp (speedStep, -1.0f, 1.0f);
+			} else if (Input.GetKeyDown (KeyCode.DownArrow) || Input.GetKeyDown (KeyCode.S)) {
+				speedStep -= 0.5f;
+				speedStep = Mathf.Clamp (speedStep, -1.0f, 1.0f);
+			} else if (Input.GetKeyDown (KeyCode.X)) {
+				speedStep = 0.0f;
+			}
+			float vertical = speedStep;
+			float horizontal = Input.GetAxis ("Horizontal");
+            //Debug.Log(horizontal);
+			float clamp = Mathf.Lerp (turnClamp, 1.0f, Mathf.Abs (vertical / 1.0f));
+			horizontal = Mathf.Clamp (horizontal, -clamp, clamp);
+			if (vertical < 0.0f) {
+				horizontal = -horizontal; // like a brake-turn.
+			}
+			leftRate = Mathf.Clamp (-vertical - horizontal, -1.0f, 1.0f);
+			rightRate = Mathf.Clamp (vertical - horizontal, -1.0f, 1.0f);
+		}
+		#endif
+
+		void FixedUpdate ()
+		{
+			// Auto Parking Brake using 'RigidbodyConstraints'.
+			if (leftRate == 0.0f && rightRate == 0.0f) {
+				float velocityMag = thisRigidbody.velocity.magnitude;
+				float angularVelocityMag = thisRigidbody.angularVelocity.magnitude;
+				if (isParkingBrake == false) {
+					if (velocityMag < autoParkingBrakeVelocity && angularVelocityMag < autoParkingBrakeVelocity) {
+						lagCount += Time.fixedDeltaTime;
+						if (lagCount > autoParkingBrakeLag) {
+							isParkingBrake = true;
+							thisRigidbody.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotationY;
+						}
+					}
+				} else {
+					if (velocityMag > autoParkingBrakeVelocity || angularVelocityMag > autoParkingBrakeVelocity) {
+						isParkingBrake = false;
+						thisRigidbody.constraints = RigidbodyConstraints.None;
+						lagCount = 0.0f;
+					}
+				}
+			} else {
+				isParkingBrake = false;
+				thisRigidbody.constraints = RigidbodyConstraints.None;
+				lagCount = 0.0f;
+			}
+			/* for reducing Calls.
+			for (int i = 0; i < rotateScripts.Length; i++) {
+				rotateScripts [i].FixedUpdate_Me ();
+			}
+			*/
+		}
+
+		void Destroy ()
+		{ // Called from "Damage_Control_CS".
+			StartCoroutine ("Disable_Constraints");
+		}
+
+		IEnumerator Disable_Constraints ()
+		{
+			// Disable constraints of MainBody's rigidbody.
+			yield return new WaitForFixedUpdate (); // This wait is required for PhysX.
+			thisRigidbody.constraints = RigidbodyConstraints.None;
+			Destroy (this);
+		}
+
+		void Get_ID_Script (ID_Control_CS tempScript)
+		{
+			idScript = tempScript;
+		}
+
+		void Pause (bool isPaused)
+		{ // Called from "Game_Controller_CS".
+			this.enabled = !isPaused;
+		}
+
+	}
+
+}
---
+++ HW10_AITank/README.md
@@ -0,0 +1,90 @@
+﻿## 坦克对战游戏 AI 设计
+
+从商店下载游戏：“Kawaii” Tank 或 其他坦克模型，构建 AI 对战坦克。具体要求：
+- 使用“感知-思考-行为”模型，建模 AI 坦克
+- 场景中要放置一些障碍阻挡对手视线
+- 坦克需要放置一个矩阵包围盒触发器，以保证 AI 坦克能使用射线探测对手方位
+- AI 坦克必须在有目标条件下使用导航，并能绕过障碍。（失去目标时策略自己思考）
+- 实现人机对战
+
+### 效果截图
+![在这里插入图片描述](https://img-blog.csdnimg.cn/20200105224241322.png?x-oss-process=image/watermark,type_ZmFuZ3poZW5naGVpdGk,shadow_10,text_aHR0cHM6Ly9ibG9nLmNzZG4ubmV0L2xpYW5neWxpbmcz,size_16,color_FFFFFF,t_70)
+![在这里插入图片描述](https://img-blog.csdnimg.cn/20200105224350665.png?x-oss-process=image/watermark,type_ZmFuZ3poZW5naGVpdGk,shadow_10,text_aHR0cHM6Ly9ibG9nLmNzZG4ubmV0L2xpYW5neWxpbmcz,size_16,color_FFFFFF,t_70)
+### 具体实现
+本次作业难度较大，根据已有的代码进行修改。
+参考博客：[坦克对战游戏 AI 设计](https://blog.csdn.net/Z_J_Q_/article/details/80732809)
+
+重要代码如下：
+#### TankAI
+TankAI类作为“感知-思考-行为”模型中的“思考”。跟踪速度和角速度随距离发生变化，距离越近速度越慢角速度越快。射线检测玩家在正前方时发射子弹，使用射线使AI坦克不会频繁发射子弹，行为更自然。
+```c#
+using System;
+using UnityEngine;
+namespace Complete
+{
+    [RequireComponent(typeof (UnityEngine.AI.NavMeshAgent))]
+    public class TankAI : MonoBehaviour
+    {
+        public UnityEngine.AI.NavMeshAgent agent { get; private set; }
+        public Transform target;
+        public float angle = 60f;
+
+        private float countTime = 0;
+        private void Start()
+        {
+            agent = GetComponentInChildren<UnityEngine.AI.NavMeshAgent>();
+            agent.updateRotation = true;
+            agent.updatePosition = true;
+        }
+
+        private void Update()
+        {
+            // 距离与40的比例
+            float ratio = (target.transform.position - transform.position).magnitude / 40;
+            // 导航速度与距离线性正相关
+            agent.speed = 2f * ratio + 1.5f;
+            // 导航角速度与距离线性负相关
+            agent.angularSpeed = 120f - 60f * ratio;
+            if (target != null)
+                agent.SetDestination(target.position);
+            // 玩家在正前方时，以0.2秒一次的频率发射子弹
+            if (countTime >= 0.2f) {
+                Ray ray = new Ray (transform.position, transform.forward);
+                RaycastHit hit;  
+                if (Physics.Raycast (ray, out hit, Mathf.Infinity)) { 
+                    if (hit.collider.gameObject.tag == "Player")
+                        GetComponent<TankShooting> ().MyShoot();
+                }
+                countTime = 0;
+            }
+            countTime += Time.deltaTime;
+        }
+    }
+}
+```
+#### TankManager
+如果对象是AI，不激活TankMovement组件，使AI坦克不能被用户控制。
+```c#
+public void EnableControl ()
+{
+    if (!AI)
+        m_Movement.enabled = true;
+    else
+        m_Instance.GetComponent<TankAI> ().enabled = true;
+    ...
+}
+```
+#### GameManager
+设置Player2坦克的TankManager的AI为true，TankAI的导航目标target为Player1的位置。
+
+```c
+private void SpawnAllTanks()
+{
+    ...
+    m_Tanks [0].AI = false;
+    m_Tanks [1].AI = true;
+    m_Tanks [1].m_Instance.GetComponent<TankAI> ().target = m_Tanks [0].m_Instance.transform;
+    m_Tanks [1].m_Instance.GetComponent<TankAI> ().enabled = false;
+}
+```
+
